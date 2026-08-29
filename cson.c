388bade35a3881bbffe8c249f9b3c58e383d71bc
Greg <gibrat.gregoire@gmail.com>
2026-08-29 12:10:19 +0200

    Ajout de la détection de racine

--- cson.c
+++ cson.c
@@ -5,9 +5,7 @@
 
 #define BUFFER_DEFAULT_SIZE 1024
 
-long getSize(FILE* file);
-int loadJson(char** dest,FILE* file);
-int whitespaceCleaner(char** str_json,long* size);
+#define DEBUG_VALUE 0
 
 // Liste de type que peux prendre un JSON
 typedef enum {
@@ -17,7 +15,8 @@ typedef enum {
     JSON_STRING,
     JSON_ARRAY,
     JSON_OBJECT,
-    JSON_DECIMAL
+    JSON_DECIMAL,
+    JSON_ERROR
 } JsonType;
 
 // Pré-déclaration
@@ -55,20 +54,81 @@ typedef struct JsonRoot{
     JsonValue root;
 }JsonRoot;
 
-int main(void){
-    char* str = (char*)calloc(46,sizeof(char));
-    strcpy(str,"{  \"name\"  : \n \"John\"  ,  \"age\"  :  30  }");
+JsonType getType(char* json);
+long getSize(FILE* file);
+int loadJson(char** dest,FILE* file);
+int whitespaceCleaner(char** str_json,long* size);
 
-    long size = 46;
-    whitespaceCleaner(&str,&size);
-    printf("%s\n",str);
+size_t _strlen(char* str);
+int _strcmp(char* str1,char* str2);
+int _strchr(char* str,char c);
+int _strcpybxy(char **dest, char *src, int x, int y);
+int _strchrxt(char * str,char x,int avoid);
+
+void debug();
+
+int main(int argc, char *argv[]){
+    if(argc != 2){
+        printf("Usage: %s <path_to_json>\n", argv[0]);
+        return 1;
+    }
+    int state; // Pour vérifiée le res d'une fonction (0 ou 1)
+    char* path = argv[1];
+    char* json_str = NULL;
+    FILE* json_file = fopen(path,"r");
+    if(json_file == NULL){
+        printf("Error occure during the opening process of the json file, check the path:\n %s\n",path);
+        return 1;
+    }
+    long size = getSize(json_file);
+    
+
+    state = loadJson(&json_str,json_file);
+    printf("Debug");
+    fclose(json_file);
+    if(state == 1){
+        printf("Error occure during the loading of the json file on RAM (File to big?)\n");
+        return 1;
+    }
+    state = whitespaceCleaner(&json_str,&size);
+
+    JsonRoot json_root;
+    json_root.root.type = getType(json_str);
+    switch(json_root.root.type){
+        case JSON_NULL:
+            json_root.root.value = NULL;
+        break;
+        case JSON_BOOL:
+            str
+        break;
+        case JSON_NUMBER:
+            printf("JSON_NUMBER\n");
+        break;
+        case JSON_STRING:
+            printf("JSON_STRING\n");
+        break;
+        case JSON_ARRAY:
+            printf("JSON_ARRAY\n");
+        break;
+        case JSON_OBJECT:
+            printf("JSON_OBJECT\n");
+        break;
+        case JSON_DECIMAL:
+            printf("JSON_DECIMAL\n");
+        break;
+        case JSON_ERROR:
+            printf("JSON_ERROR\n");
+        break;
+    }
 
     return 0;
 }
 
+
+
 int loadJson(char** dest,FILE* file){
     // Détermine la longeur du JSON
-    long size = getSize(file);
+    long size = getSize(file); //Rewind -> Retour au debut
     if(size == -1){
         return 1;
     }
@@ -85,20 +145,19 @@ int loadJson(char** dest,FILE* file){
             return 1;
         }
     }
-
     // On lit tout le fichier et le décale dans dest
     long read = fread(*dest,1,size,file);
     if(read < size){
         return 1;
     }
-    *(dest) [size] = '\0';
+    (*dest)[size] = '\0';
     return 0;
 }
 
 int whitespaceCleaner(char** str_json,long* size){
     int whitespaceCleanedCount = 0;
     int index = 0;
-    int IsInsideQuote = 0;
+    int IsInsideQuote = 0;  
 
     for(long i = 0 ;i<(*size);i++){
         char c = (*str_json)[i];
@@ -140,9 +199,125 @@ int whitespaceCleaner(char** str_json,long* size){
     return 0;
 }
 
+JsonType getType(char* json){
+    if(json == NULL){
+        return JSON_ERROR;
+    }
+    switch(json[0]){
+        case 't':
+        case 'f':
+            return JSON_BOOL;
+        break;
+        case '-':
+            return JSON_DECIMAL;
+        break;
+        case 'n':
+            return JSON_NULL;
+        break;
+        case '"':
+            return JSON_STRING;
+        break;
+        case '[':
+            return JSON_ARRAY;
+        break;
+        case '{':
+            return JSON_OBJECT;
+        break;
+        default:
+            return JSON_NUMBER;
+    }
+    return JSON_ERROR;
+}
+
 long getSize(FILE* file){
     fseek(file, 0, SEEK_END);
     long size = ftell(file);
     rewind(file);
     return size;
+}
+
+////////////////////////////////////////////////////
+
+size_t _strlen(char* str){
+    size_t i =0;
+    while(str[i] != '\0'){
+        i++;
+    }
+    return i;
+}
+
+int _strcmp(char* str1,char* str2){
+    size_t size_1 = _strlen(str1);
+    size_t size_2 = _strlen(str2);
+    if(size_1 != size_2){
+        return 1;
+    }
+    for(size_t i=0;i<size_1;i++){
+        if(str1[i] != str2[i]){
+            return 1;
+        }
+    }
+    return 0;
+}
+
+int _strchr(char* str,char c){
+    size_t size = _strlen(str);
+    for(size_t i = 0;i<size;i++){
+        if(str[i] == c){
+            return (int)i;
+        }
+    }
+    return -1;
+}
+
+// String copy between x and y
+int _strcpybxy(char **dest, char *src, int x, int y){
+    int lenSrc = strlen(src);
+    if (x < 0 || y >= lenSrc || y < x) {
+        return 1;
+    }
+
+    size_t size = y - x + 2;
+    if (*dest == NULL) {
+        *dest = malloc(size);
+    } else {
+        *dest = realloc(*dest, size);
+    }
+    if (*dest == NULL) {
+        return 1;
+    }
+
+    int index = 0;
+    for (int i = x; i <= y; i++) {
+        (*dest)[index] = src[i];
+        index++;
+    }
+
+    (*dest)[index] = '\0';
+    return 0;
+}
+
+// String reserch x time
+int _strchrxt(char * str,char x,int avoid){
+    int xTime = 0;
+    int size = (int)_strlen(str);
+    for(int i=0;i<size;i++){
+        if(str[i] == x && avoid == xTime){
+            return i;
+        }
+        if(str[i] == x) {
+            xTime++;
+        }
+    }   
+    return -1;
+}
+
+////////////////////////////////////////////////////
+
+void debug(){
+    if(DEBUG_VALUE == 1){
+        printf("\033[38;2;255;0;0m");
+        printf("DEBUG!\n");
+        printf("\033[0m");
+    }
 }
